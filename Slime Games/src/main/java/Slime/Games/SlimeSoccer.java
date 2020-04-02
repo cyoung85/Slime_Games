8f191a06480dcefe3f6f4f2d3398cfb8fdcafaeb
Bluegamer0 <nickherr6@gmail.com>
2020-04-02 14:25:33 -0400

    Add files via upload

--- Slime Games/src/main/java/Slime/Games/SlimeSoccer.java
+++ Slime Games/src/main/java/Slime/Games/SlimeSoccer.java
@@ -1,5 +1,39 @@
 package Slime.Games;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class SlimeSoccer {
 
+import javax.swing.*;
+public class SlimeSoccer implements ActionListener{
+	private JFrame frame;
+	private JButton play;
+	private JTextArea test;
+	
+	@SuppressWarnings("deprecation")
+	public SlimeSoccer() {
+		frame = new JFrame("Slime Soccer");
+		Dimension screenSize = new Dimension(1200,700);
+		frame.setPreferredSize(screenSize);
+		frame.setResizable(false);
+		
+		
+		
+		
+		frame.setLayout(new BorderLayout());
+		//frame.add(play,BorderLayout.SOUTH);
+		//frame.add(test,BorderLayout.NORTH);
+	    frame.pack();
+	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+		
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		SlimeGames.resetScore();
+		//test.setText(test.getText().concat("You have clicked the button\n"));
+	}
 }
